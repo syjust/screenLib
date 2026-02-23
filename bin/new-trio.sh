1903ea0dfd7b4bec6479bac5eb9b4f3d64a6cc5b
Sylvain Just <sylvain@justdevops.com>
2026-02-23 14:16:34 +0100

    feat: accept optional dir/prefix args in new-trio

--- bin/new-trio.sh
+++ bin/new-trio.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-default_folder="$PWD"
-read -e -p "Folder (default: '$default_folder'): " folder
-[[ -z "$folder" ]] && folder="$default_folder"
-folder="${folder/#\~/$HOME}"
-if [[ ! -d "$folder" ]] || [[ ! -r "$folder" ]] || [[ ! -x "$folder" ]]; then
-    echo "Error: '$folder' is not a readable/traversable directory" >&2
+if [[ ! -z "$1" ]] ; then
+    dir="$1"
+else
+    default_dir="$PWD"
+    read -e -p "Folder (default: '$default_dir'): " dir
+    [[ -z "$dir" ]] && dir="$default_dir"
+    dir="${dir/#\~/$HOME}"
+fi
+if [[ ! -d "$dir" ]] || [[ ! -r "$dir" ]] || [[ ! -x "$dir" ]]; then
+    echo "Error: '$dir' is not a readable/traversable directory" >&2
     exit 1
 fi
 
-default_prefix="$(basename "$folder" | sed 's/ +/-/g')"
-read -r -p "Tab prefix (default: '$default_prefix'): " prefix
-[[ -z "$prefix" ]] && prefix="$default_prefix"
+if [[ ! -z "$2" ]] ; then
+    prefix="$2"
+else
+    default_prefix="$(basename "$dir" | sed 's/ +/-/g')"
+    read -r -p "Tab prefix (default: '$default_prefix'): " prefix
+    [[ -z "$prefix" ]] && prefix="$default_prefix"
+fi
 
-dir="$folder"
 session="$STY"
 
 current_window=$(screen -S "$session" -Q number 2>/dev/null | awk '{print $1}')
