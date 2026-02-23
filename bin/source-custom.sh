96cb47f8844b17180f063b68fab8f6db892293e1
Sylvain Just <sylvain@justdevops.com>
2026-02-23 16:20:58 +0100

    refact: centralize paths via setenv in main.screenrc

--- bin/source-custom.sh
+++ bin/source-custom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for f in "$HOME/.screen/custom/"*.screenrc; do
+for f in "${HOME_SCREEN_DIR:-$HOME/.screen}/custom/"*.screenrc; do
     [ -f "$f" ] && screen -S "$STY" -X source "$f"
 done
